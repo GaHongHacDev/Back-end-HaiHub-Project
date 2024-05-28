af0d786b9a23a6a3ed249cbbc967434f52cf8839
ChauHien2912 <chauhien2912@gmail.com>
2024-05-28 14:53:07 +0700

    add gitgone

--- Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
+++ Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2c932d6c750fd20e02cff9b724c068c44c41cd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a6bff1ea6fd937f70534b9c3a02b84d03a2eea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
