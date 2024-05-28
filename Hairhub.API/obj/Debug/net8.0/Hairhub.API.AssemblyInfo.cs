b71843306c8254cbc5d3a142c71eca2d2faa6dce
ChauHien2912 <chauhien2912@gmail.com>
2024-05-28 14:49:05 +0700

    crud config

--- Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
+++ Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b967b2f4f6493f295daec608b5f085873e9603da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fa1ea7e641890804b95243f36cb67137d6555af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
