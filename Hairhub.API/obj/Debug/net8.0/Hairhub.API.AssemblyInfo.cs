788308d5f69cb2897553575931df25bbd382b214
Ga Honghac <flamingotran1532002@gmail.com>
2024-05-28 21:40:40 +0700

    Create docker compose

--- Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
+++ Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("33edbbd3-c969-461d-a4ef-ad3fdc245882")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e954229cc7abac50d70edbd7881e74e07429f64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d596f32073d6cacb5721e58bf88d512d87b7cca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
+++ Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e954229cc7abac50d70edbd7881e74e07429f64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d596f32073d6cacb5721e58bf88d512d87b7cca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
