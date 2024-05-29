ad153ec714bc1630b996c72b095c632fd66d22ba
Trung Tuan <tuandtse172284@fpt.edu.vn>
2024-05-29 17:15:04 +0700

    change br

--- Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
+++ Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56e713d697121ec9cf1f10666b583a84cd36f3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d47517dd5d36f01dfdbc8f303a3dc9a4a3ef6942")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
