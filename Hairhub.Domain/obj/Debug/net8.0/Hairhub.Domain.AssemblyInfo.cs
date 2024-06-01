08c5ad164399297371e69e0a53ef8badf3d97b7d
Ga Honghac <flamingotran1532002@gmail.com>
2024-06-02 03:00:31 +0700

    commit dev before merge Tien

--- Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
+++ Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b7e95ee6335ad90ebe8c60a8170d0fa9513a4cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e8395cc4b55ba2441a02845b3725e22bf405f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
