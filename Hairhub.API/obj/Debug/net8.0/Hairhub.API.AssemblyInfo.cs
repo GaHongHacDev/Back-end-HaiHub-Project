3a80bcb1e4d02a8b99ec1c7ea77dd580d8b55779
Ga Honghac <flamingotran1532002@gmail.com>
2024-06-03 10:03:12 +0700

    push new code define default page size and page number

--- Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
+++ Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4e4c4a0418e654cfcd735825f473739f3b855d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+113bd31f91d417ec1a3f210a8e04074c080093fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
