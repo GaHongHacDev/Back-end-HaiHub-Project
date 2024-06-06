42c26638b4fe09521c0434a1453c7fa400ab8d0c
Trung Tuan <tuandtse172284@fpt.edu.vn>
2024-06-06 07:05:57 +0700

    fix error

--- Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
+++ Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+563f7af3d8ac89b2dcc8fdfa73210767c535a11d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0a928e8e618fa968ed4971979d7ebc0310406e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
