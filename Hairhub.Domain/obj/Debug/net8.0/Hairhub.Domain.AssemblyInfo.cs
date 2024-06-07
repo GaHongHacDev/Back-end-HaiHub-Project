0fd14808854f434140d74c52c8d1397efaf906aa
GaHongHacDev <tientranxuan263@gmail.com>
2024-06-07 20:43:34 +0700

    merge trungtuan to master

--- Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
+++ Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd9e8760b18158358f1161980b56dabe4222bf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd33cd3d1ca163d1a0d9c108b15adfe2d65addb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
