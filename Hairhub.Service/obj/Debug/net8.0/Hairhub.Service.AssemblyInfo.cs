e6156a925de5c544d08a158f95b9bdf6fbbd02bf
Trung Tuan <tuandtse172284@fpt.edu.vn>
2024-06-20 22:05:42 +0700

    crud approval

--- Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
+++ Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4956236828fd602ed352f1a238be93d217906f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67ed2cf75b807d97fa33af3d4e9ad27f3c17d95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
