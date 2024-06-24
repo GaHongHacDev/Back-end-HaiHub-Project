79ed456aa54222bb6191c16cdbd84469cc2403e0
GaHongHacDev <tientranxuan263@gmail.com>
2024-06-24 21:41:35 +0700

    update register account api

--- Hairhub.Domain/Dtos/Requests/Accounts/CreateAccountRequest.cs
+++ Hairhub.Domain/Dtos/Requests/Accounts/CreateAccountRequest.cs
@@ -4,9 +4,13 @@ namespace Hairhub.Domain.Dtos.Requests.Accounts
 {
     public class CreateAccountRequest
     {
+        [Required]
         public string RoleName { get; set; }
+        [Required]
         public string Phone { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string FullName { get; set; }
     }
 }
