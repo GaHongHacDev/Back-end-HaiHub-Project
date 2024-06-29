e37f3ef86dba52447daaaacb6da75e7622226ddd
GaHongHacDev <tientranxuan263@gmail.com>
2024-06-29 23:09:28 +0700

    commit code create api appointment

--- Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
+++ Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b5196e57826d165aec128c12b308f0288b98bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a29b0df0967404aa2910406ebb5b94da471ac05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
