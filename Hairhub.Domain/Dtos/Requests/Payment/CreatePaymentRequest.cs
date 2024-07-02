fb9310bb92236247f68bf813b724f90c3dd5fb22
GaHongHacDev <tientranxuan263@gmail.com>
2024-07-02 21:16:28 +0700

    commit master

--- Hairhub.Domain/Dtos/Requests/Payment/CreatePaymentRequest.cs
+++ Hairhub.Domain/Dtos/Requests/Payment/CreatePaymentRequest.cs
@@ -12,28 +12,5 @@ namespace Hairhub.Domain.Dtos.Requests.Payment
         public Guid ConfigId { get; set; }
         public Guid SalonOWnerID { get; set; }
         public string? Description { get; set; }
-
-
-
-        public Config Configs { get; set; }
-
-        public SalonOwners SalonOwner { get; set; }
-    }
-
-    public class Config
-    {
-        public Guid Id { get; set; }
-
-        public string PackageName { get; set; }
-        public decimal PackageFee { get; set; }
-    }
-
-    public class SalonOwners
-    {
-        public Guid Id { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public string? Phone { get; set; }
-        public string? Address { get; set; }
     }
 }
