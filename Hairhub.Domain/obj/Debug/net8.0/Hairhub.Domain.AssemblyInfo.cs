dfdcd2896cf14959ebb2c521d633fca3673221ea
GaHongHacDev <tientranxuan263@gmail.com>
2024-08-09 00:14:20 +0700

    commit to merge ChauHien

--- Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
+++ Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad3570a76760006eb16596b69c426cb6f4c7015")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+302d4188d640a172f037bc0fa12c834cef741ba9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
