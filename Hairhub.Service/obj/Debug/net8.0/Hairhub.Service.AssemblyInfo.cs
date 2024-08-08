6831beec8f33d4498afe39693a52f0bd69047beb
GaHongHacDev <tientranxuan263@gmail.com>
2024-08-08 10:53:43 +0700

    commit deploy server

--- Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
+++ Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f044c396cfe7daae469df3b965dd9cd084a4be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d598d8351dcd1fc4ddbfad2efb86ca9e09d9e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
