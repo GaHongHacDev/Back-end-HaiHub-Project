e489ea76d980abe09ea6ae500601a45cb8dd4e51
GaHongHacDev <tientranxuan263@gmail.com>
2024-08-09 16:37:08 +0700

    update database

--- Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
+++ Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9d61655a33f0e7955e9fbbf0d005838f075662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7395c0f04fbd3d77268b68e63c54d53cf658b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
