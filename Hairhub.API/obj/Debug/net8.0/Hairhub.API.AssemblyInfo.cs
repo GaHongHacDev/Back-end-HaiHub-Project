abbe8c0e00c9bf9251a05288d6408b3a359a1ec6
GaHongHacDev <tientranxuan263@gmail.com>
2024-08-11 01:48:01 +0700

    commit

--- Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
+++ Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7395c0f04fbd3d77268b68e63c54d53cf658b89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5501838d5784549d5b0b73f767a07c2c568d150")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
+++ Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e489ea76d980abe09ea6ae500601a45cb8dd4e51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5501838d5784549d5b0b73f767a07c2c568d150")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
