016b529b581228374ca82f9946415d3348d53d64
chauh <Chauhien2912@gmail.com>
2024-08-16 10:36:06 +0700

    tesst ddi a

--- Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
+++ Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff80e5e03fa5867fa1d004db2c243607948e929f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be5fbd8f7e76e04afcf89ec3f6d6fe6ff9e96bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
