2f991105de7757ed2745ff007feb58bb60739e61
GaHongHacDev <tientranxuan263@gmail.com>
2024-08-27 17:18:21 +0700

    commit deploy

--- Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
+++ Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+544b00cd347f9758ce86b0670be86aef29a87ff8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc6b490e27a39fe1921e1e6ae7e28fa126ae83f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
