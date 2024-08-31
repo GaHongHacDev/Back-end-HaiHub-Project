192d15ae7cb0842c50baa5e5dfc26288d42e3249
chauh <Chauhien2912@gmail.com>
2024-08-31 21:00:42 +0700

    update program

--- Hairhub.API/Program.cs
+++ Hairhub.API/Program.cs
@@ -79,13 +79,7 @@ builder.Services.AddSignalR();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: CorsConstant.PolicyName,
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5173") // Thay thế với domain của FE
-                  .AllowAnyHeader()
-                  .AllowAnyMethod()
-                  .AllowCredentials();
-        });
+        policy => { policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod(); });
 });
 
 // Jwt configuration starts here
--- Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
+++ Hairhub.Service/obj/Debug/net8.0/Hairhub.Service.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86bebf690fca5eb61734fb3415983abe5ca33b0f")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad5d588c9580da1a600965d414c88b844fb0bcd")]
->>>>>>> 7e8205d5bc61426edcee203a214a999b74eeffd0
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9342f97ef0298143d54bc374e2e8bb6908fc2114")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
