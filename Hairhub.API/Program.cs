156add99983dfc457f35191120cbadf6d0718b46
chauh <Chauhien2912@gmail.com>
2024-09-01 20:24:40 +0700

    fix program

--- Hairhub.API/Program.cs
+++ Hairhub.API/Program.cs
@@ -79,7 +79,7 @@ builder.Services.AddSignalR();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: CorsConstant.PolicyName,
-        policy => { policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod(); });
+        policy => { policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod(); });
 });
 
 // Jwt configuration starts here
