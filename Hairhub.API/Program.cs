32b12efba5d4f94956bafdb12a4c2285be74eafc
chauh <Chauhien2912@gmail.com>
2024-09-05 14:26:02 +0700

    fixx include origin

--- Hairhub.API/Program.cs
+++ Hairhub.API/Program.cs
@@ -91,7 +91,13 @@ builder.Services.AddSignalR();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: CorsConstant.PolicyName,
-        policy => { policy.WithOrigins("http://localhost:5173", "https://www.hairhub.com.vn").AllowAnyHeader().AllowAnyMethod(); });
+        policy => {
+            policy.SetIsOriginAllowed(origin =>
+                origin == "http://localhost:5173" ||
+                origin == "https://www.hairhub.com.vn")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
 });
 
 // Jwt configuration starts here
