d6b72ac8c44f477c5303b9f38ad44bf1d2e8ae8c
chauh <Chauhien2912@gmail.com>
2024-09-05 16:22:14 +0700

    fix v6 SignalR

--- Hairhub.API/Controllers/SignalRHubController.cs
+++ Hairhub.API/Controllers/SignalRHubController.cs
@@ -22,13 +22,15 @@ namespace Hairhub.API.Controllers
         public async Task<IActionResult> BroadcastMessage([FromBody] BroadcastMessageRequest request)
         {
             var timestamp = DateTime.Now;
-            await _hubContext.Clients.All.SendAsync("ReceiveMessage", request.Message, timestamp, request.ServiceId);
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", request.Message, request.DateAppointment, timestamp, request.ServiceId);
             return NoContent();
         }
     }
     public class BroadcastMessageRequest
     {
         public string Message { get; set; }
+
+        public string DateAppointment { get; set; }
         public List<string> ServiceId { get; set; }
     }
 }
