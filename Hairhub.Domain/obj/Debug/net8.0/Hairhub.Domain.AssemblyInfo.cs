b7c057d64a0ae9d8d0c16548c41e8f809851cb9b
GaHongHacDev <tientranxuan263@gmail.com>
2024-09-20 12:04:44 +0700

    Update Salon table in total_rating to decimal value type

--- Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
+++ Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcaa48c214fe95471ad899f61e6b006c56b20013")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abaf22c3e25f5f9aec7025539952f70595b895c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
