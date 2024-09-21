6c276fe665ea1bb8f1b4b5bf1847f220b77fd514
GaHongHacDev <tientranxuan263@gmail.com>
2024-09-21 20:58:34 +0700

    commit pull ChauHien

--- Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
+++ Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3368a84719037d631df6387dceb43b7f819d898a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61fc4fd2cb628864d4f66779f381ea8290eb25c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
