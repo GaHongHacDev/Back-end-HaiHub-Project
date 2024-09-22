b387c987b89e235ad0615e1351443765f9193e4d
chauh <Chauhien2912@gmail.com>
2024-09-22 22:39:36 +0700

    new code

--- Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
+++ Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69d6d434ca038e8d83756151207ce5a19ab4d94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f678b47bc3e46569d04a07b74cfd4492aae62a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
