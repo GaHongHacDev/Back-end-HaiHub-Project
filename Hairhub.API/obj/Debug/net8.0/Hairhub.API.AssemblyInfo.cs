1292deb62650bd3c3eff675407341f62e774f629
GaHongHacDev <tientranxuan263@gmail.com>
2024-10-03 21:09:27 +0700

    commit

--- Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
+++ Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69d6d434ca038e8d83756151207ce5a19ab4d94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2366fb17f9a34e6fbddabe4a2a419eb7fc87ac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
