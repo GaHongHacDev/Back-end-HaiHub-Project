fc01d222f551703c876731607ba1c8957b2ac425
chauh <Chauhien2912@gmail.com>
2024-10-04 10:20:49 +0700

    new code

--- Hairhub.Infrastructure/Data/HaiHubDbContext.cs
+++ Hairhub.Infrastructure/Data/HaiHubDbContext.cs
@@ -26,7 +26,7 @@ namespace Hairhub.Infrastructure
                 .Build();
             //optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"));
 
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("HienConnectionString"));
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DockerConnectionString"));
 
         }
 
