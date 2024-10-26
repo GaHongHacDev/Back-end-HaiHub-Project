5bf15356cae0aa441ac24e7011e3e5d20e03a804
chauh <Chauhien2912@gmail.com>
2024-10-27 02:55:35 +0700

    new code

--- Hairhub.Domain/Dtos/Responses/Payment/PaymentReportResponse.cs
+++ Hairhub.Domain/Dtos/Responses/Payment/PaymentReportResponse.cs
@@ -8,47 +8,37 @@ namespace Hairhub.Domain.Dtos.Responses.Payment
 {
     public class PaymentReportResponse
     {
+        public Guid Id { get; set; }
+        public string beneficiary { get; set; }
+        public string NumberAccount { get; set; }
+        public string BankName { get; set; }
         public string? ReasonCancle { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime? ConfirmDate { get; set; }
-        public string FullName { get; set; }
-        public string NumberAccount { get; set; }
-        public string BankName { get; set; }
-        public decimal Balance { get; set; }
-        public string Status { get; set; }
-
-        public PaymentInformation Payment { get; set; }
-    }
-
-    public class PaymentInformation{
-        public Guid Id { get; set; }
-        public Guid? AccountId { get; set; }
-        public decimal TotalAmount { get; set; }
-        public DateTime? PaymentDate { get; set; }
-        public string PaymentType { get; set; }
+        public DateTime? PaymentDate { get; set; }  
         public string? Description { get; set; }
-        public string Status { get; set; }
-        public string? PaymentCode { get; set; } 
+        public string? typeofpayment { get; set; }
+        public string? statusofpayment { get; set; }
+        public decimal? Balance { get; set; }
+        public string? Status { get; set; }
 
-        public SalonOwnerInformation salon {  get; set; }
-
-        public CustomerInformation Customer { get; set; }
+        public List<string> urlPaymentImage { get; set; }
+        public AccountInformation AccountInformation { get; set; }
     }
-
-    public class SalonOwnerInformation
+    public class AccountInformation
     {
-        public string Phone { get; set; }
+        public Guid? Id { get; set; }
+
+        public Guid? UserId { get; set; }
         public string FullName { get; set; }
 
-        public string? Email { get; set; }
+        public string Email { get; set; }   
+
+        public string Phone {  get; set; }
+
+        public string urlImage { get; set; }
+
     }
 
-    public class CustomerInformation
-    {
-        public string Phone { get; set; }
-        public string FullName { get; set; }
-
-        public string? Email { get; set; }
-    }
 
 }
--- Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
+++ Hairhub.Infrastructure/obj/Debug/net8.0/Hairhub.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f390be6cdf29eb892dc171a820dcf225b451591")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e683d16ff5d864aba520c15e38100fa5f3d7c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
