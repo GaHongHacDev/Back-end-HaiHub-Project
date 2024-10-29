8dbab635d77376b394ff6cd6e1d8317d05a25a45
GaHongHacDev <tientranxuan263@gmail.com>
2024-10-29 20:45:05 +0700

    create getAppointmentByStatus API

--- Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
+++ Hairhub.API/obj/Debug/net8.0/Hairhub.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b95d5ce5136d5b28bdde52c7ab7dc16ad97f585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8dad12dffe2948e25c0d0c930d79c5669cb9f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
