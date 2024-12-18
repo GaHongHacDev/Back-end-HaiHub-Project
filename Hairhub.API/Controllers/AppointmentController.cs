0b80e7757186236caa4b90bb440e8ae3a30838c0
GaHongHacDev <tientranxuan263@gmail.com>
2024-12-18 17:56:36 +0700

    commit code push server

--- Hairhub.API/Controllers/AppointmentController.cs
+++ Hairhub.API/Controllers/AppointmentController.cs
@@ -123,7 +123,7 @@ namespace Hairhub.API.Controllers
 
         [HttpGet]
         [Route("{salonId:Guid}")]
-        //[Authorize(Roles = RoleNameAuthor.Admin + "," + RoleNameAuthor.SalonOwner)]
+        [Authorize(Roles = RoleNameAuthor.Admin + "," + RoleNameAuthor.SalonOwner)]
         public async Task<IActionResult> FrequentlyCustomers(
             [FromRoute] Guid salonId,
             [FromQuery] string? time,
