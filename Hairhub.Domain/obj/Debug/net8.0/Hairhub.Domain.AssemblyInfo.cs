2f627c5e8664fac2bba42cdec4a15dcad887663a
chauh <Chauhien2912@gmail.com>
2024-12-19 21:32:39 +0700

    rateofreturnedcustomer and cost average on product

--- Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
+++ Hairhub.Domain/obj/Debug/net8.0/Hairhub.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be6cd8922c9937207e8b7e0a70937e9f1408c86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4249b4df030a97fe4bc84a74817d8a941c835a60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hairhub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
