091715c91ea1d771c8a209dee91365eae722b2ea
GaHongHacDev <tientranxuan263@gmail.com>
2024-12-27 13:41:44 +0700

    commit

---
+++ Hairhub.Domain/Dtos/Responses/Appointments/GetCompileAppointmentAdmin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hairhub.Domain.Dtos.Responses.Appointments
+{
+    public class GetCompileAppointmentAdmin
+    {
+        public List<CompileAppointmentByDayOfWeek> CompileAppointmentByDayOfWeek { get; set; }
+        public double SuccessedAppointmentPercent { get; set; }
+        public double CancelAppointmentPercent { get; set; }
+        public double FailedAppointmentPercent { get; set; }
+    }
+
+    public class CompileAppointmentByDayOfWeek
+    {
+        public string DayOfWeek { get; set; }
+        public long NumberOfSuccessedAppointment { get; set; }
+    }
+}
